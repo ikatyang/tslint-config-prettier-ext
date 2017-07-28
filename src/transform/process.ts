b22789b1bbd48bbf25fe7f5fdb07b0ef79774458
ikatyang <ikatyang@gmail.com>
2017-07-28 21:26:33 +0800

    Merge branch 'master' into support-tslint-misc-rules

--- src/transform/process.ts
+++ src/transform/process.ts
@@ -8,25 +8,34 @@ export function process(source_text: string, source_path: string) {
 
     const rule_name = path.basename(__dirname);
 
+    let message_before;
+    let message_after;
+
     it('should fail before formatting', () => {
-      let message;
-      expect(function () {
-        try {
-          runtime.lint(filename, source);
-        } catch (error) {
-          message = error.message;
-          throw error;
-        }
-      }).toThrowErrorMatchingSnapshot();
-      expect(message).toMatch(new RegExp('ERROR: \\\\(' + rule_name + '\\\\)'));
+      try {
+        runtime.lint(filename, source);
+      } catch (error) {
+        message_before = error.message;
+      }
+      expect(message_before).toMatch(new RegExp('ERROR: \\\\(' + rule_name + '\\\\)'));
     });
 
     const formatted = runtime.format(source);
 
-    it('should pass after formatting', () => {
-      expect(function () {
+    it('should affect error message after formatting', () => {
+      try {
+        message_after = 'no error\\n';
         runtime.lint(filename, formatted);
-      }).not.toThrowError();
+      } catch (error) {
+        message_after = error.message;
+      }
+      expect(message_after).not.toBe(message_before);
+      expect(
+        '\\n<<<<<< before\\n' +
+        message_before +
+        '\\n======\\n' +
+        message_after +
+        '\\n>>>>>> after\\n').toMatchSnapshot();
     });
 
     it('should be pretty after formatting', () => {
